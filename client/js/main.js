bfe6f07d571996eb3aa56c70c21def41e19bec01
Martijn Hoogerland <martijn091@gmail.com>
2013-11-27 21:22:57 +0100

    Added register

---
+++ client/js/main.js
@@ -0,0 +1,26 @@
+if (Meteor.isClient) {
+  Meteor.Router.add({
+    '/': 'home_screen',
+    '/home': 'home_screen',
+    '/backend': 'back_end'
+  });
+
+  Meteor.Router.filters({
+    requireLogin: function(page) {
+      if (Meteor.user()) {
+        return page;
+      } else {
+        return 'login_screen';
+      }
+    }
+  });
+
+  Meteor.Router.filter('requireLogin');
+
+  Meteor.startup( function() {
+	filepicker.setKey("Auj9wAFhRKW7YkHRdY1Zwz");
+	filepicker.constructWidget(document.getElementById('attachment'));
+  });
+
+  /* test */
+}
